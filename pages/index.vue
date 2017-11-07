719c6273419081f196a4dfebe15515996f38ed9a
ronnidc (RDC) <ronnidc@gmail.com>
2017-11-07 21:49:01 +0100

    url

--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,7 @@
       </h2>
       <div class="article-links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
+        <a href="https://github.com/ronnidc/nuxtvue" target="_blank" class="button--grey">GitHub</a>
       </div>
   </div>
 </template>
